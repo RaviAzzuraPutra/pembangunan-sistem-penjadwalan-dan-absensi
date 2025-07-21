ecb5389812c7cab1b0fd7baa472d0b545798effc
RaviAzzuraPutra <ravi.azzura74@gmail.com>
2025-07-21 19:18:56 +0700

    >

--- front-end/next.config.mjs
+++ front-end/next.config.mjs
@@ -7,6 +7,7 @@ const nextConfig = {
 export default withPWA({
     dest: "public",
     register: true,
+    disable: process.env.NODE_ENV === "development",
     skipWaiting: true,
     swSrc: 'src/sw.js',
 })(nextConfig);
