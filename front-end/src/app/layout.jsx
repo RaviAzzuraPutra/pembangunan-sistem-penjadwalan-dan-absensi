63db8973cf83da312feaad8784745b1ed0f2883b
RaviAzzuraPutra <ravi.azzura74@gmail.com>
2025-07-22 05:47:03 +0700

    .

--- front-end/src/app/layout.jsx
+++ front-end/src/app/layout.jsx
@@ -18,7 +18,6 @@ export default function RootLayout({ children }) {
         <link rel="manifest" href="/manifest.json" />
       </head>
       <body>
-        <ServiceWorkerRegistration />
         {children}
       </body>
     </html>
