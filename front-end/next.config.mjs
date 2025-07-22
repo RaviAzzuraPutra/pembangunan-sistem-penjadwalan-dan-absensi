561b00f24a1de73856c159abffd677eef0e1f7e9
RaviAzzuraPutra <ravi.azzura74@gmail.com>
2025-07-22 08:43:33 +0700

    masih belum kedaftar sw.js nya

--- front-end/next.config.mjs
+++ front-end/next.config.mjs
@@ -2,10 +2,25 @@
 import withPWA from 'next-pwa';
 
 const nextConfig = {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
+    reactStrictMode: true,
+    pwa: {
+        dest: 'public',
+        disable: process.env.NODE_ENV === 'development',
+        runtimeCaching,
+        buildExcludes: [
+            /chunks\/images\/.*$/, // Don't precache files under .next/static/chunks/images this improves next-optimized-images behaviour
+            /chunks\/pages\/api\/.*/, // Dont cache the API it needs fresh serverinfo
+        ],
+        exclude: [
+            /\.map$/, // dont cache map files
+            /^.*ts.*$/, // Dont let serviceworker touch the TS streams
+            /-manifest.json$/, // exclude those pesky json files in _next root but still serve the ones we need from /_next/static
+        ],
+        skipWaiting: true, // installs new SW when available without a prompt, we only need to send a reload request to user.
+        dynamicStartUrl: false, // recommend: set to false if your start url always returns same HTML document, then start url will be precached, this will help to speed up first load.
+        reloadOnOnline: false, // Prevents reloads on offline/online switch
+        sourcemap: false,
+    },
 };
 
 export default withPWA(nextConfig);
